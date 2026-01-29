1ee8f8890ed588772077e57314aff281fbf81ec3
Mackwell <mackwellm@gmail.com>
2026-01-29 09:16:12 +0200

    Your descriptive commit message

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 import requests
 import pytz
+import os
+from datetime import *
 
 #Calling the football prediction API
-API_KEY = 'https://football-prediction-api.p.rapidapi.com/api/v2/list-markets'
+API_KEY = "https://football-prediction-api.p.rapidapi.com/api/v2/list-markets"
 
+api_timezone = pytz.timezone("Europe/Madrid")
+local_timezone = pytz.timezone("Africa/Johannesburg")
+
+response = requests.get(API_KEY)
+
+def get_current_datetime():
+    madrid = datetime.now(tz=timezone.utc).astimezone(api_timezone)
+    return madrid
+
+print(get_current_datetime())
+#___________________________________________UI____________________________________________
